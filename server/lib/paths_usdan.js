cbf79f4e536eefe66ccc716ef13dd675338884f5
Andrea <ziyuqiu@brandeis.edu>
2015-09-18 15:30:59 -0400

    paths_libraryeast splited

---
+++ server/lib/paths_usdan.js
@@ -0,0 +1,233 @@
+paths_usdan = [
+	{
+	    "start":"library_c03",
+	    "end":"usdan_c10",
+	    "description": "Walk along the road beside the library",
+	},
+	{
+	    "start":"usdan_c10",
+	    "end":"library_c03",
+	    "description": "Walk along the road beside the library",
+	},
+	{
+	    "start":"library_c04",
+	    "end":"usdan_c10",
+	    "description": "Walk along the road beside the library",
+	},
+	{
+	    "start":"usdan_c10",
+	    "end":"library_c04",
+	    "description": "Walk along the road beside the library",
+	},
+	{
+		"start":"usdan_c08",
+		"end":"usdan_c06",
+		"description": "Walk straight towards to the loop road after get out of the usdan",
+	},
+	{
+		"start":"usdan_c06",
+		"end":"usdan_c08",
+		"description": "Walk straight towards Usdan",
+	},
+	{
+		"start":"usdan_c08",
+		"end":"usdan_c09",
+		"description": "Turn to the left-hand side and walk along the road",
+	},
+	{
+		"start":"usdan_c09",
+		"end":"usdan_c08",
+		"description": "Walk towards Usdan, keeping loop road on the left",
+	},
+	{
+		"start":"usdan_c08",
+		"end":"usdan_c05",
+		"description": "Turn to the right-hand side and walk along the road",
+	},
+	{
+		"start":"usdan_c05",
+		"end":"usdan_c08",
+		"description": "Walk straight towards Usdan",
+	},
+	{
+		"start":"usdan_c09",
+		"end":"usdan_c07",
+		"description": "Walk towards the loop road",
+	},
+	{
+		"start":"usdan_c07",
+		"end":"usdan_c09",
+		"description": "Walk towards the Libray, keeping loop road at back",
+	},
+	{
+		"start":"usdan_c09",
+		"end":"rabb_c01",
+		"description": "Walk towards the road on the left, and walk along to the loop road",
+	},
+	{
+		"start":"rabb_c01",
+		"end":"usdan_c09",
+		"description": "Walk towards Usdan, keeping Rabb at back",
+	},
+
+	{
+	    "start":"rosenstiel_c03",
+	    "end":"usdan_c01",
+	    "description": "Castle should be on your right, walk to the 3-way intersection",
+	},
+	{
+	    "start":"usdan_c01",
+	    "end":"rosenstiel_c03",
+	    "description":"Keeping the castle on your left, walk down the main road."  
+	},
+	{
+	    "start":"usdan_c01",
+	    "end":"usdan_c02",
+	    "description": "Take either the left road, or the stairs straight ahead to Usdan",
+	},
+	{
+	    "start":"usdan_c02",
+	    "end":"usdan_c01",
+	    "description":"Keep Usdan behind you on your left and walk along the main road downhill in the direction of the castle."
+	},
+	{
+	    "start":"rubenstein_c16",
+	    "end":"usdan_c01",
+	    "description": "Take the left road down to the next intersection with castle on your left",
+	},
+	{
+	    "start":"usdan_c01",
+	    "end":"rubenstein_c16",
+	    "description": "With the castle on your right, continue up to the next intersection with loop road.",
+	},
+	{
+	    "start":"usdan_c02",
+	    "end":"usdan_c03",
+	    "description": "Continue walking with Usdan on your right.",
+	},
+	{
+	    "start":"usdan_c03",
+	    "end":"usdan_c02",
+	    "description":"Continue walking with Usdan on your left."    
+	},
+	{
+	    "start":"usdan_c03",
+	    "end":"usdan_c04",
+	    "description": "Walk past the statue and under the overhead building bridge to Usdan.",
+	},
+	{
+	    "start":'usdan_c04',
+	    "end":"usdan_c03",
+	    "description":"Go right under the overhead building bridge, keeping right and going past the gray statue."
+	},
+	{
+	    "start":"usdan_c02",
+	    "end":"usdan_c04",
+	    "description": "Walk left past the statue and under the overhead building bridge to Usdan",
+	},
+	{
+	    "start":"usdan_c04",
+	    "end":"usdan_c02",
+	    "description":"Keep left, walking under the overhead building bridge and past the statue."  
+	},
+		{
+	    "start":"usdan_e01",
+	    "end":"usdan_c04",
+	    "description": "Exit Usdan and turn left.",
+	},
+	{
+	    "start":"usdan_c04",
+	    "end":"usdan_e01",
+	    "description": "The entance is on your right.",
+	},
+	{
+	    "start":"schwartz_e01",
+	    "end":"usdan_c03",
+	    "description": "Exit the building and trun left.",
+	},
+	{
+	    "start":"usdan_c03",
+	    "end":"schwartz_e01",
+	    "description": "Walk toward the building with many stairs.",
+	},
+	{
+	    "start":"schwartz_e01",
+	    "end":"usdan_c02",
+	    "description": "Exit the building and trun right.",
+	},
+	{
+	    "start":"usdan_c02",
+	    "end":"schwartz_e01",
+	    "description": "Walk toward the building with many stairs.",
+	},
+	{
+	    "start":"bassine_c03",
+	    "end":"brown_e01",
+	    "description": "Walk along the small and this the first building on your right.",
+	},
+	{
+	    "start":"brown_e01",
+	    "end":"bassine_c03",
+	    "description": "Turn left and walk along the small path.",
+	},
+	{
+	    "start":"schwartz_c01",
+	    "end":"brown_e01",
+	    "description": "Walk along the smaller path and the entrance is your left.",
+	},
+	{
+	    "start":"brown_e01",
+	    "end":"schwartz_c01",
+	    "description": "Walk along the small path toward the larger path.",
+	},
+	{
+	    "start":"usdan_c04",
+	    "end":"levin_e02",
+	    "description": "The entrance is on your left",
+	},
+	{
+	    "start":"levin_e02",
+	    "end":"usdan_c04",
+	    "description": "Exit the building and turn right.",
+	},
+	{
+	    "start":"levin_e01",
+	    "end":"usdan_c04",
+	    "description": "Exit the building and turn right.",
+	},	
+	{
+	    "start":"usdan_c04",
+	    "end":"levin_e01",
+	    "description": "The entrance is on your left.",
+	},
+	{
+	    "start":"usdan_c08",
+	    "end":"levin_e01",
+	    "description": "Walk toward the building and the entrance is on your right.",
+	},
+	{
+	    "start":"levin_e01",
+	    "end":"usdan_c08",
+	    "description": "Exit the building and trun left.",
+	},
+	{
+	    "start":"usdan_c08",
+	    "end":"usdan_e01",
+	    "description": "Walk toward the building and the entrance is on your left.",
+	},
+	{
+	    "start":"usdan_e01",
+	    "end":"usdan_c08",
+	    "description": "Exit the building and trun right.",
+	},
+	{
+	    "start":"usdan_c10",
+	    "end":"usdan_c09",
+	    "description": "Walk along the small path toward the bus stop.",
+	},
+	{
+	    "start":"usdan_c09",
+	    "end":"usdan_c10",
+	    "description": "Walk along the small path toward the library.",
+	}
+]
