b6e4cbf8070b06a81797d6833f0eacda947246a3
Jessica Huynh <jhuynh37@brandeis.edu>
2015-10-08 21:57:37 -0400

    parking lot Polygons and entrances

--- server/lib/locations_parking.js
+++ server/lib/locations_parking.js
@@ -3,11 +3,12 @@ locations_parking = [
 		"id":"athleticslot",
 		"name":"Athletics Parking Lot",
 		"nickname":"Athletics Lot",
-		"coordinates":[
-			new Point(42.365088,-71.256173), 
-			new Point(42.364422,-71.257879),
-			new Point(42.364272,-71.257766),
-			new Point(42.364914,-71.256057), ],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.365088,-71.256173], 
+			[42.364422,-71.257879],
+			[42.364272,-71.257766],
+			[42.364914,-71.256057]]
+		]},
 		"category":["parking"],
 		"entrances":["athleticslot_e01","athleticslot_e02"],
 		"area":["athletics","athletic complex"]
@@ -16,11 +17,12 @@ locations_parking = [
 		"id":"gosmanparkinglot",
 		"name":"Gosman Parking Lot",
 		"nickname":"Gosman Lot",
-		"coordinates":[
-			new Point(42.365056,-71.253832), 
-			new Point(42.365541,-71.252837),
-			new Point(42.365393,-71.252676),
-			new Point(42.364858,-71.253718), ],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.365056,-71.253832], 
+			[42.365541,-71.252837],
+			[42.365393,-71.252676],
+			[42.364858,-71.253718]]
+		]},
 		"category":["parking"],
 		"entrances":["gosmanparkinglot_e01","gosmanparkinglot_e02"],
 		"area":["athletics","athletic complex"]
@@ -29,11 +31,12 @@ locations_parking = [
 		"id":"linseyparkinglot",
 		"name":"Linsey Parking Lot",
 		"nickname":"Linsey Lot",
-		"coordinates":[
-			new Point(42.365568,-71.254114), 
-			new Point(42.365449,-71.253988), 
-			new Point(42.365645,-71.253642), 
-			new Point(42.365739,-71.253857)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.365568,-71.254114], 
+			[42.365449,-71.253988], 
+			[42.365645,-71.253642], 
+			[42.365739,-71.253857]]
+		]},
 		"category":["parking"],
 		"entrances":["linseyparkinglot_e01"],
 		"area":["athletics","athletic complex"]
@@ -42,12 +45,13 @@ locations_parking = [
 		"id":"towerlot",
 		"name":"Tower Parking Lot",
 		"nickname":"T-Lot",
-		"coordinates":[
-			new Point(42.370790, -71.258274), 
-			new Point(42.370909, -71.258726), 
-			new Point(42.370091, -71.259134), 
-			new Point(42.369904, -71.258646),
-			new Point(42.369990, -71.258506)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.370790, -71.258274], 
+			[42.370909, -71.258726], 
+			[42.370091, -71.259134], 
+			[42.369904, -71.258646],
+			[42.369990, -71.258506]]
+		]},
 		"category":["parking"],
 		"entrances":["towerlot_e01", "towerlot_e02"],
 		"area":["upper campus","mandel"]
@@ -56,11 +60,12 @@ locations_parking = [
 		"id":"librarylot",
 		"name":"Library Parking lot",
 		"nickname":"Library Lot",
-		"coordinates":[
-			new Point(42.369333, -71.258163), 
-			new Point(42.369143, -71.258346), 
-			new Point(42.368845, -71.257814), 
-			new Point(42.368924, -71.257598)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.369333, -71.258163], 
+			[42.369143, -71.258346], 
+			[42.368845, -71.257814], 
+			[42.368924, -71.257598]]
+		]},
 		"category":["parking"],
 		"entrances":["librarylot_e01", "librarylot_e02"],
 		"area":["upper campus","libraries"]
@@ -69,11 +74,12 @@ locations_parking = [
 		"id":"kutzlot",
 		"name":"Kutz Parking Lot",
 		"nickname":"Kutz Lot",
-		"coordinates":[
-			new Point(42.368797, -71.256007), 
-			new Point(42.368885, -71.256314), 
-			new Point(42.368675, -71.256459), 
-			new Point(42.368580, -71.256135)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.368797, -71.256007], 
+			[42.368885, -71.256314], 
+			[42.368675, -71.256459], 
+			[42.368580, -71.256135]]
+		]},
 		"category":["parking"],
 		"entrances":["kutzlot_e01"],
 		"area":["upper campus"]
@@ -82,12 +88,13 @@ locations_parking = [
 		"id":"northquadlot",
 		"name":"North Quad Parking Lot",
 		"nickname":"North Quad Parking Lot",
-		"coordinates":[
-			new Point(42.370431, -71.256786),
-			new Point(42.370306, -71.256963),
-			new Point(42.370211, -71.256866), 
-			new Point(42.369640, -71.254723),
-			new Point(42.369763, -71.254697)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.370431, -71.256786],
+			[42.370306, -71.256963],
+			[42.370211, -71.256866], 
+			[42.369640, -71.254723],
+			[42.369763, -71.254697]]
+		]},
 		"category":["parking"],
 		"entrances":["northquadlot_e01", "northquadlot_e02"],
 		"area":["upper campus","north"]
@@ -96,11 +103,12 @@ locations_parking = [
 		"id":"sciencevisitorlot",
 		"name":"Science Visitor Parking Lot",
 		"nickname":"Science Visitor Parking Lot",
-		"coordinates":[
-			new Point(42.366635, -71.257073), 
-			new Point(42.366314, -71.257357), 
-			new Point(42.366187, -71.257086), 
-			new Point(42.366530, -71.256810)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.366635, -71.257073], 
+			[42.366314, -71.257357], 
+			[42.366187, -71.257086], 
+			[42.366530, -71.256810]]
+		]},
 		"category":["parking"],
 		"entrances":["sciencevisitorlot_e01"],
 		"area":["upper campus","science complex"]
@@ -109,11 +117,12 @@ locations_parking = [
 		"id":"sciencespeciallot",
 		"name":"Science Special Access Parking Lot",
 		"nickname":"Science Special Access Parking Lot",
-		"coordinates":[
-			new Point(42.366639, -71.257279), 
-			new Point(42.366364, -71.257566), 
-			new Point(42.366308, -71.257368), 
-			new Point(42.366636, -71.257060)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.366639, -71.257279], 
+			[42.366364, -71.257566], 
+			[42.366308, -71.257368], 
+			[42.366636, -71.257060]]
+		]},
 		"category":["parking"],
 		"entrances":["sciencespeciallot_e01"],
 		"area":["upper campus","science complex"]
@@ -122,11 +131,12 @@ locations_parking = [
 		"id":"sciencelot",
 		"name":"Science Parking Lot",
 		"nickname":"Science Parking Lot",
-		"coordinates":[
-			new Point(42.366284, -71.256297), 
-			new Point(42.366137, -71.256153), 
-			new Point(42.366481, -71.255527), 
-			new Point(42.366629, -71.255677)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.366284, -71.256297], 
+			[42.366137, -71.256153], 
+			[42.366481, -71.255527], 
+			[42.366629, -71.255677]]
+		]},
 		"category":["parking"],
 		"entrances":["sciencelot_e01","sciencelot_e02"],
 		"area":["upper campus","science complex"]
@@ -135,11 +145,12 @@ locations_parking = [
 		"id":"stonemanlot",
 		"name":"Stoneman Parking Lot",
 		"nickname":"Stoneman Parking Lot",
-		"coordinates":[
-			new Point(42.366284, -71.254980), 
-			new Point(42.366829, -71.254753), 
-			new Point(42.366836, -71.254822), 
-			new Point(42.366331, -71.255020)],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.366284, -71.254980], 
+			[42.366829, -71.254753], 
+			[42.366836, -71.254822], 
+			[42.366331, -71.255020]]
+		]},
 		"category":["parking"],
 		"entrances":["stonemanlot_e01"],
 		"area":["upper campus"]
@@ -148,12 +159,13 @@ locations_parking = [
 		"id":"eastquadlot",
 		"name":"East Quad Parking Lot",
 		"nickname":"East Quad Parking Lot",
-		"coordinates":[
-			new Point(42.367559, -71.254144), 
-			new Point(42.367339, -71.254362), 
-			new Point(42.367139, -71.254406), 
-			new Point(42.366950, -71.254004),
-			new Point(42.367311, -71.253783),],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.367559, -71.254144], 
+			[42.367339, -71.254362], 
+			[42.367139, -71.254406], 
+			[42.366950, -71.254004],
+			[42.367311, -71.253783]]
+		]},
 		"category":["parking"],
 		"entrances":["eastquadlot_e01","eastquadlot_e02"],
 		"area":["upper campus","east"]
@@ -162,23 +174,23 @@ locations_parking = [
 		"id":"charlesriverlot",
 		"name":"Charles River Lot",
 		"nickname":"C-Lot",
-		"coordinates":[
-			new Point(42.359381, -71.258471),
-			new Point(42.359402, -71.258644),
-			new Point(42.359345, -71.258710),
-			new Point(42.359387, -71.259058),
-			new Point(42.360113, -71.259882),
-			new Point(42.360318, -71.259650),
-			new Point(42.360366, -71.259618),
-			new Point(42.360383, -71.259398),
-			new Point(42.360321, -71.259344),
-			new Point(42.360267, -71.258557),
-			new Point(42.360233, -71.258358),
-			new Point(42.360294, -71.258064),
-			new Point(42.360028, -71.257795),
-			new Point(42.359894, -71.258162),
-			new Point(42.359378, -71.258467)
-		],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.359381, -71.258471],
+			[42.359402, -71.258644],
+			[42.359345, -71.258710],
+			[42.359387, -71.259058],
+			[42.360113, -71.259882],
+			[42.360318, -71.259650],
+			[42.360366, -71.259618],
+			[42.360383, -71.259398],
+			[42.360321, -71.259344],
+			[42.360267, -71.258557],
+			[42.360233, -71.258358],
+			[42.360294, -71.258064],
+			[42.360028, -71.257795],
+			[42.359894, -71.258162],
+			[42.359378, -71.258467]]
+		]},
 		"category":["parking"],
 		"entrances":["clot_e01","clot_e02","clot_e03"],
 		"area":["lower campus","charles river"]
@@ -187,44 +199,46 @@ locations_parking = [
 		"id":"bernsteinparkinglot",
 		"name":"Bernstein-Marcus Parking Lot",
 		"nickname":"Bernstein Marcus parking lot",
-		"coordinates":[
-			new Point(42.364969, -71.260228),
-			new Point(42.364806, -71.260081),
-			new Point(42.364895, -71.259868),
-			new Point(42.365064, -71.260004)
-		],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.364969, -71.260228],
+			[42.364806, -71.260081],
+			[42.364895, -71.259868],
+			[42.365064, -71.260004]]
+		]},
 		"category":["parking"],
 		"nearby":["bernstein-marcus","stop_admissions","greatlawn","slosberg"],
+		"entrances":["bernsteinparkinglot_e01"],
 		"area":["lower campus"]
 	},
 	{
 		"id":"hlot",
 		"name":"Hassenfeld Parking Lot",
 		"nickname":"H-Lot",
-		"coordinates":[
-			new Point(42.366973, -71.261853),
-			new Point(42.366777, -71.262181),
-			new Point(42.366365, -71.262398),
-			new Point(42.366215, -71.262355),
-			new Point(42.366190, -71.262189),
-			new Point(42.366453, -71.261501)
-		],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.366973, -71.261853],
+			[42.366777, -71.262181],
+			[42.366365, -71.262398],
+			[42.366215, -71.262355],
+			[42.366190, -71.262189],
+			[42.366453, -71.261501]]
+		]},
 		"description":"Parking for vehicles bearing red permit . Violators towed at owener's expense.",
 		"category":["parking"],
 		"nearby":["shapirodorm"],
+		"entrances":["hlot_e01","hlot_e02"],
 		"area":["lower campus"]
 	},
 	{
 		"id":"admissionsvisitorparking",
 		"name":"Admissions Visitor Parking Lot",
 		"nickname":"Admissions visitor parking",
-		"coordinates":[
-			new Point(42.364566, -71.260030),
-			new Point(42.364576, -71.260094),
-			new Point(42.364164, -71.260265),
-			new Point(42.364085, -71.260076),
-			new Point(42.364454, -71.259875)
-		],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.364566, -71.260030],
+			[42.364576, -71.260094],
+			[42.364164, -71.260265],
+			[42.364085, -71.260076],
+			[42.364454, -71.259875]]
+		]},
 		"category":["parking"],
 		"description":"Parking for Admissions visitors ONLY. Violators towed at owner's expense.",
 		"nearby":["slosberg","admissions","bernstein-marcus","stop_admissions","ridgewoodA"],
@@ -235,29 +249,29 @@ locations_parking = [
 		"id":"theaterparking",
 		"name":"Theater Lot",
 		"nickname":"Theater lot",
-		"coordinates":[
-			new Point(42.364363, -71.263228),
-			new Point(42.363935, -71.261710),
-			new Point(42.363467, -71.261968),
-			new Point(42.363715, -71.263009),
-			new Point(42.363438, -71.263228),
-			new Point(42.363594, -71.263761),
-		],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.364363, -71.263228],
+			[42.363935, -71.261710],
+			[42.363467, -71.261968],
+			[42.363715, -71.263009],
+			[42.363438, -71.263228],
+			[42.363594, -71.263761]]
+		]},
 		"category":["parking"],
 		"nearby":["spingold,ridgewoodB,zivB,zivA,lemberg,goldman"],
-		"entrances":[],
+		"entrances":["theaterlot_e01","theaterlot_e02","theaterlot_e03","theaterlot_e04"],
 		"area":["lower campus"]
 	},
 	{
 		"id":"hellerparking",
 		"name":"Heller Parking Lot",
 		"nickname":"Heller lot",
-		"coordinates":[
-			new Point(42.369277, -71.260469),
-			new Point(42.369111, -71.260026),
-			new Point(42.369042, -71.260051),
-			new Point(42.369221, -71.260525),
-		],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.369277, -71.260469],
+			[42.369111, -71.260026],
+			[42.369042, -71.260051],
+			[42.369221, -71.260525]]
+		]},
 		"category":["parking"],
 		"nearby":["schneider","harlan"],
 		"entrances":["hellerparkinglot_e01", "hellerparkinglot_e02"],
@@ -268,16 +282,16 @@ locations_parking = [
 		"id":"southresidenceparking",
 		"name":"South Residence Parking Lot",
 		"nickname":"South Residence lot",
-		"coordinates":[
-			new Point(42.363529, -71.262857),
-			new Point(42.363089, -71.263212),
-			new Point(42.362842, -71.262506),
-			new Point(42.363140, -71.261921),
-			new Point(42.363258, -71.261857),
-		],
+		"coordinates": {"type":"Polygon","coordinates":[
+			[[42.363529, -71.262857],
+			[42.363089, -71.263212],
+			[42.362842, -71.262506],
+			[42.363140, -71.261921],
+			[42.363258, -71.261857]]
+		]},
 		"category":["parking"],
-		"nearby":["ridgewoodB,zivB,zivA,lemberg"],
-		"entrances":[],
+		"nearby":["ridgewoodB","zivB","zivA","lemberg"],
+		"entrances":["southlot_e01","southlot_e02"],
 		"area":["lower campus"]
 	},
 
