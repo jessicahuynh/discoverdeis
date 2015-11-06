28d7d1da3def12983276dbcbdfedbd4330f49c78
Jessica Huynh <jhuynh37@brandeis.edu>
2015-11-05 20:40:21 -0500

    set Session variable outside callback

--- client/compatibility/currentLoc.js
+++ client/compatibility/currentLoc.js
@@ -2,6 +2,7 @@ navigator.geolocation.watchPosition(function (position) {
 	var current = new Point(position.coords.latitude, position.coords.longitude);
 	Session.setPersistent("currentLocation", current);
 	
+	var inLoc = null;
 	Meteor.call("searchLocations",
 		Session.get("currentLocation"),
 		function (error, data) {
@@ -9,10 +10,11 @@ navigator.geolocation.watchPosition(function (position) {
 				console.log(error);
 			}
 			else {
-				Session.setPersistent("inLocation", data);
+				inLoc = data;
 			}
 		}	
 	);
+	Session.setPersistent("inLocation",inLoc);
 	
 	if (Session.get("currentLocation").x == null || Session.get("currentLocation").x == undefined) {
 	    $("#permissionsAlert").show();
