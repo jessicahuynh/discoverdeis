1a1b2004737cb968bf25b490e641404559eb1d06
tim <tjhickey@brandeis.edu>
2015-11-06 16:21:56 -0500

    changed meteor methods

--- server/server.js
+++ server/server.js
@@ -1,3 +1,97 @@
+pointIncluded = function(vertices, current) {
+	included = false;
+	numVert = vertices.length;
+
+	// create arrays of the x and y coordinates of the polygon
+	var xArray = [];
+	for (var i = 0; i < numVert; i++) {
+		xArray.push(vertices[i][0]);
+	}
+	
+	var yArray = [];
+	for (var i = 0; i < numVert; i++) {
+		yArray.push(vertices[i][1]);
+	}
+
+	for (var i = 0, j = numVert - 1; i < numVert; j = i++) {
+		if ((yArray[i] >= current.y) != (yArray[j] >= current.y) &&
+			(current.x <= (xArray[j] - xArray[i]) * 
+				(current.y - yArray[i]) / (yArray[j] - yArray[i]) +
+				xArray[i])
+			) {
+			included = !included;
+
+		}
+	}
+
+	return included;
+};
+
+distance = function(start, end) {
+	var theNearestDistance = 1000000000;
+	//console.log(JSON.stringify(start)+JSON.stringify(end));
+	var R = 6371000; // metres
+
+	var lat1 = start.x;
+	var lat2 = end.x;
+	var lon1 = start.y;
+	var lon2 = end.y;
+
+	var p1 = lat1 * (Math.PI / 180);
+	var p2 = lat2 * (Math.PI / 180);
+	var dp = (lat2-lat1) * (Math.PI / 180);
+	var dl = (lon2-lon1) * (Math.PI / 180);
+
+	var a = Math.sin(dp/2) * Math.sin(dp/2) +
+	        Math.cos(p1) * Math.cos(p2) *
+	        Math.sin(dl/2) * Math.sin(dl/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	var d = R * c;
+	//console.log(d);
+
+	return d;
+}
+
+
+searchLocations = function(current) {
+	var location = null;
+	var locatedHere = null;
+    console.log("SL:"+ JSON.stringify(current));
+
+	for (var i = 0; i < Locations.find().count(); i++) {
+		// if the given Point is in the location, return the location
+		if (pointIncluded(Locations.find().fetch()[i].coordinates.coordinates[0],current))
+		{ return [Locations.find().fetch()[i], "in"]; }
+	}
+			
+	if (!locatedHere) {
+		var nearestPoint = CornerPoints.find({
+			"coordinate":{
+				$near: {
+					$geometry: {
+						"type": "Point" ,
+						"coordinates": [ current.y , current.x ]
+					}
+				}
+			}
+		});		
+		var theList = nearestPoint.fetch();
+		var theNearest = nearestPoint.fetch()[0];
+		for(var i=0; i<theList.length; i++){
+			console.log(JSON.stringify(theList[i]));
+			//console.log(JSON.stringify(Locations.findOne({"name":theList[i].name})));
+			console.log("-----");
+		}
+
+		
+		var n = distance(current,{"x":theNearest.coordinate.coordinates[0],"y":theNearest.coordinate.coordinates[1]});
+		return [Locations.findOne({"name":theNearest.name}),"near",n];
+		
+	}
+}
+
+
 Meteor.methods({
 	/* numVert: number of vertices
 	 * vertices: array of vertices
@@ -5,126 +99,15 @@ Meteor.methods({
 	 *
 	 * Determines if a given Point is located within a polygon of n vertices
 	 * Algorithm from http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html */
-	pointIncluded: function(vertices, current) {
-		included = false;
-		numVert = vertices.length;
-
-		// create arrays of the x and y coordinates of the polygon
-		var xArray = [];
-		for (var i = 0; i < numVert; i++) {
-			xArray.push(vertices[i][0]);
-		}
-		
-		var yArray = [];
-		for (var i = 0; i < numVert; i++) {
-			yArray.push(vertices[i][1]);
-		}
-
-		for (var i = 0, j = numVert - 1; i < numVert; j = i++) {
-			if ((yArray[i] >= current.y) != (yArray[j] >= current.y) &&
-				(current.x <= (xArray[j] - xArray[i]) * 
-					(current.y - yArray[i]) / (yArray[j] - yArray[i]) +
-					xArray[i])
-				) {
-				included = !included;
-
-			}
-		}
-	
-		return included;
-	},
+	pointIncluded: pointIncluded,
 
 	/* Search through all locations to see where you are */
-	searchLocations: function(current) {
-		var location = null;
-		var locatedHere = null;
-
-		for (var i = 0; i < Locations.find().count(); i++) {
-			// if the given Point is in the location, return the location
-			Meteor.call("pointIncluded",
-				Locations.find().fetch()[i].coordinates.coordinates[0],
-				current,
-				function(error, data) {
-					if (error) {
-						console.log(error);
-					}
-					else {
-						locatedHere = data;
-						
-						if (locatedHere) {
-							location = [Locations.find().fetch()[i], "in"];
-							return location;
-						}
-					}
-			});
-		}
-						
-		if (!locatedHere) {
-			var nearestPoint = CornerPoints.find({
-				"coordinate":{
-					$near: {
-						$geometry: {
-							"type": "Point" ,
-							"coordinates": [ current.y , current.x ]
-						}
-					}
-				}
-			});		
-			var theNearest = nearestPoint.fetch()[0];
-
-			var theNearestDistance = 1000000000;
-			Meteor.call("distance",
-				current,
-				{"x":theNearest.coordinate.coordinates[0],"y":theNearest.coordinate.coordinates[1]},
-				function(error,data) {
-					if (error) {
-						console.log(error);
-					}
-					else {
-						theNearestDistance = data;
-						if (theNearestDistance < 1000000) {	
-							var n = [Locations.findOne({"name":theNearest.name}),"near",theNearestDistance];
-							console.log("near " + Locations.findOne({"name":theNearest.name}).name);
-							return n;
-						}
-						else {
-							console.log("off campus");
-							return null;
-						}
-					}
-				}
-			);
-
-		}
-	},
+	searchLocations: searchLocations, 
 
 
 	/* returns the distance between two points 
 	* adapted from http://www.movable-type.co.uk/scripts/latlong.html */
-	distance: function(start, end) {
-		//console.log(JSON.stringify(start)+JSON.stringify(end));
-		var R = 6371000; // metres
-
-		var lat1 = start.x;
-		var lat2 = end.x;
-		var lon1 = start.y;
-		var lon2 = end.y;
-
-		var p1 = lat1 * (Math.PI / 180);
-		var p2 = lat2 * (Math.PI / 180);
-		var dp = (lat2-lat1) * (Math.PI / 180);
-		var dl = (lon2-lon1) * (Math.PI / 180);
-
-		var a = Math.sin(dp/2) * Math.sin(dp/2) +
-		        Math.cos(p1) * Math.cos(p2) *
-		        Math.sin(dl/2) * Math.sin(dl/2);
-		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-		var d = R * c;
-		//console.log(d);
-
-		return d;
-	}
+	distance: distance
 
 	
 
